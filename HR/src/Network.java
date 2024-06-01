9f4b602435a97562f6f98bed57e1a4f39c152892
tomer cohen <128282969+tomeraf@users.noreply.github.com>
2024-06-01 20:21:12 +0300

    Delete HR directory

--- HR/src/Network.java
+++
@@ -1,6 +0,0 @@
-import java.util.*;
-public class Network {
-    private List<Branch> BranchList;
-
-    public List<Branch> getBranchList(){return BranchList;}
-}
--- HR/src/Pair.java
+++
@@ -1,8 +0,0 @@
-public class Pair<T,E> {
-    public T first;
-    public E second;
-    public Pair(T first,E second) {
-        this.first=first;
-        this.second=second;
-    }
-}
